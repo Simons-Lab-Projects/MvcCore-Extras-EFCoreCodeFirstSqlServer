3f519326a1822ea856a3f1b6a188d9a3236e5e07
Simon Woodworth <s.woodworth@ucc.ie>
2020-09-08 18:14:49 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using EFCoreCodeFirstSqlServer.Models;
+
+namespace EFCoreCodeFirstSqlServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: /Home
+        public ViewResult Index()
+        {
+            ViewBag.Time = DateTime.Now.ToLocalTime();
+            return View();
+        }
+
+        //GET: /Home/Foo
+        public string Foo()
+        {
+            return "Hello from Foo";
+        }
+
+        public string GetDb()
+        {
+            string returnString = "";
+            string NL = System.Environment.NewLine;
+
+            using (var db = new BloggingContext())
+            {
+                // Create
+                returnString += "Inserting a new blog" + NL;
+                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                db.SaveChanges();
+
+                // Read
+                returnString += "Querying for a blog" + NL;
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+
+                // Update
+                returnString += "Updating the blog and adding a post" + NL;
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(
+                    new Post
+                    {
+                        Title = "Hello World",
+                        Content = "I wrote an app using EF Core!"
+                    });
+                db.SaveChanges();
+
+                // Delete
+                returnString += "Delete the blog" + NL;
+                //db.Remove(blog);
+                //db.SaveChanges();
+            }
+
+            return returnString;
+
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreCodeFirstSqlServer.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Server=database-2.cma5ondcixlz.eu-west-1.rds.amazonaws.com;Database=BloggingDB;User Id=admin;Password=sqlserver2020;");
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
